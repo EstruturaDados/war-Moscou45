824e785b972e2f6afd7b52f617231dca5ae74886
Lucas Rodrigues Borges <156516672+Moscou45@users.noreply.github.com>
2025-11-14 00:22:31 +0000

    laço principal e cadastro dos continentes

--- guerra2.c
+++ guerra2.c
@@ -24,3 +24,44 @@ void limparBufferEntrada(){
     while ((c = getchar()) != '\n' && c != EOF);
 }
 
+// ---laço principal do Menu ----   
+
+int main(){
+    struct Territorio paises[MAX_TERRITORIO];
+    int total_territorios = 0;
+    int opcao;
+
+    do {
+
+        printf("======================================\n");
+        printf("Vamos cadastrar os 5 territorios iniciais do nosso mundo.\n");
+
+        // cadastro dos continentes
+
+        while (total_territorios < MAX_TERRITORIO){
+            int i =1;
+
+            printf("\n--- Cadastro do territorio %d---\n", i +total_territorios);
+
+            printf("Nome do territorio: ");
+            fgets(paises[total_territorios].nome, TAM_STRING, stdin);
+
+            printf("Cor do Exercito (ex: azul,verde): ");
+            fgets(paises[total_territorios].cor, TAM_STRING, stdin);
+
+            paises[total_territorios].nome[strcspn(paises[total_territorios].nome, "\n")] = '\0';
+
+            paises[total_territorios].cor[strcspn(paises[total_territorios].cor, "\n")] = '\0';
+
+            printf("Numero de tropas: ");
+            sscanf("&d", &paises[total_territorios].tropas);
+            limparBufferEntrada();
+
+            //i++;
+            total_territorios++;
+
+        }
+
+    } while (opcao != 0);
+    return 0;
+} 
